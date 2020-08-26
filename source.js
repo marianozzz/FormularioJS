4f5036546db55becc3c5cde25595f126ec52425a
marianozzz <mariano.zanier@gmail.com>
2020-08-26 18:57:33 -0300

    validaciones

--- source.js
+++ source.js
@@ -1,13 +1,49 @@
-
 function validacion()
 {
    //obteniendo el valor que se puso en campo text del formulario
-   var name = document.getElementById("nombre").value;
+   var nombre = document.getElementById("nombre").value;
+   var apellido = document.getElementById("apellido").value;
+   var email = document.getElementById("email").value;
    //la condición
+ 
+  if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+    document.getElementById('alert').removeAttribute("hidden"); 
+    //document.getElementById('alert').hidden = false;
+        
+    //con el return false evito que se recargue el form
+    return false;
+  }
+
+  if ( nombre.length > 20){
+    document.getElementById('alert').removeAttribute("hidden");
+    document.getElementById('alert').textContent =" El nombre no puede superar los 20 caracteres";
+    return false;
+  }
+  //Campo Apellido
+
+  if (apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
+     document.getElementById('alert-2').removeAttribute("hidden"); 
+    //document.getElementById('alert').hidden = false;
+        
+    //con el return false evito que se recargue el form
+    return false;
+  }
+
+/****************/
+/*Validar Email */
+/****************/
+
+if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
+  
+  document.getElementById('alert-email').removeAttribute("hidden");
+  document.getElementById('alert-email').textContent ="Escriba una direccion validad de email";
+  return false;
+}
+  /*
    if (name.length == 0) {
      //  return false;
        alert("hola mundo");
    }
-   return true;
+   return true;*/
    
 }
