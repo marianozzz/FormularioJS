b76c76098e98b4b8d9870722b51a3ab3666d9971
marianozzz <mariano.zanier@gmail.com>
2020-08-26 19:08:13 -0300

    inputs validados

--- source.js
+++ source.js
@@ -1,27 +1,21 @@
 function validacion()
 {
    //obteniendo el valor que se puso en campo text del formulario
-<<<<<<< HEAD
+
    var nombre = document.getElementById("nombre").value;
    var apellido = document.getElementById("apellido").value;
    var email = document.getElementById("email").value;
-   //la condición
+
+
  
   if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
-=======
-   var valor = document.getElementById("nombre").value;
-   //la condición
- 
-  if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
->>>>>>> c05771d4e24a3eebf671c9c33aa43786ad140627
+
     document.getElementById('alert').removeAttribute("hidden"); 
-    //document.getElementById('alert').hidden = false;
         
     //con el return false evito que se recargue el form
     return false;
   }
 
-<<<<<<< HEAD
   if ( nombre.length > 20){
     document.getElementById('alert').removeAttribute("hidden");
     document.getElementById('alert').textContent =" El nombre no puede superar los 20 caracteres";
@@ -47,8 +41,7 @@ if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
   document.getElementById('alert-email').textContent ="Escriba una direccion validad de email";
   return false;
 }
-=======
->>>>>>> c05771d4e24a3eebf671c9c33aa43786ad140627
+
   /*
    if (name.length == 0) {
      //  return false;
