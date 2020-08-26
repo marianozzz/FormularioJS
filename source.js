c05771d4e24a3eebf671c9c33aa43786ad140627
marianozzz <mariano.zanier@gmail.com>
2020-08-26 02:33:34 -0300

    validacion

--- source.js
+++ source.js
@@ -2,12 +2,22 @@
 function validacion()
 {
    //obteniendo el valor que se puso en campo text del formulario
-   var name = document.getElementById("nombre").value;
+   var valor = document.getElementById("nombre").value;
    //la condición
+ 
+  if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+    document.getElementById('alert').removeAttribute("hidden"); 
+    //document.getElementById('alert').hidden = false;
+        
+    //con el return false evito que se recargue el form
+    return false;
+  }
+
+  /*
    if (name.length == 0) {
      //  return false;
        alert("hola mundo");
    }
-   return true;
+   return true;*/
    
 }
